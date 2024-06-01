b463dd56f10e38b6365c147822485108be260f21
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2024-06-01 12:49:41 +1200

    [#1] Use headless chrome in CICD For running system tests in GitHub Actions.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,9 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV['IS_CI']
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 end
